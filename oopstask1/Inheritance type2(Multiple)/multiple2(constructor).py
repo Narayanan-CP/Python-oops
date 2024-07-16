441efbaaaaa81c9c945ed6c2019806172f5a558f
narayanan.cp.official@gmail.com <narayanan.cp.official@gmail.com>
2024-07-16 22:59:45 +0530

    changed

--- oopstask1/Inheritance type2(Multiple)/multiple2(constructor).py
+++ oopstask1/Inheritance type2(Multiple)/multiple2(constructor).py
@@ -13,5 +13,6 @@ class Child(Parent):
         print(self.Networth//2)     
                
 child1=Child(100000)
+#we are using print because we are printing the variable.If we are calling the methid means we dont want to use the print
 print(child1.Networth) 
 child1.Childnetworth()                   
