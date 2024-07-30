12e497e0e1df973213565842316aa8a8cd82ad3b
narayanan.cp.official@gmail.com <narayanan.cp.official@gmail.com>
2024-07-30 12:52:55 +0530

    changed

--- oopstask1/getter and setter/2.py
+++ oopstask1/getter and setter/2.py
@@ -1,13 +1,15 @@
 class Person:
     def __init__(self):
-        #In private variable we can ADD the name
-        # only by using public method and it is accesible only inside the class.
+        #If it is a public variable means we can ADD the name at outside by specifying it,so when we use getter and setter we make variables as private
+        # so only we use private method which is accesible only inside the class.
         self.__name=None
+        #setter
     def SetName(self,value):
         if(len(value)>20):
             print("please enter lesser value")
         else:
-            self.__name=value 
+            self.__name=value
+        #getter     
     def GetName(self):
         if self.__name==None:
             print("Please Enter valueable name")
